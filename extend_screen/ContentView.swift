3f4fd471c1cd46ede319b02d6cba03788462501f
YanKe <yankeing@foxmail.com>
2026-06-28 17:18:22 +0800

    Initial Commit

---
+++ extend_screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  extend_screen
+//
+//  Created by linke on 2026/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ extend_screen/extend_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  extend_screenApp.swift
+//  extend_screen
+//
+//  Created by linke on 2026/6/28.
+//
+
+import SwiftUI
+
+@main
+struct extend_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
